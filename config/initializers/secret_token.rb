35f8006cbd24ec428ececb6f0016df071a608def
Dan Pickett <dan.pickett@launchware.com>
2013-07-23 11:03:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Launchazon::Application.config.secret_key_base = '4549b9c82136007bfa105f9cb083aa00fb97bb03a12857a5a15205b830fac90dca6484f510b796ce1dd50df4d0c3e711ba7c0ed99817b9a7611186c5b9911f5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Launchazon::Application.config.session_store :cookie_store, key: '_launchazon_session'
